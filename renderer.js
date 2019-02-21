3d7e3f593f4b359744c89c9cda01e45a0c5a8bf6
TS000 <tylersnell@me.com>
2019-02-21 10:19:10 -0700

    render text on keyup

--- renderer.js
+++ renderer.js
@@ -7,3 +7,11 @@ const openFileButton = document.querySelector('#open-file');
 const saveMarkdownButton = document.querySelector('#save-markdown');
 const revertButton = document.querySelector('#revert');
 const saveHtmlButton = document.querySelector('#save-html');
+
+const renderMarkdown = (markdown) => {
+  htmlView.innerHTML = marked(markdown, { sanitize: true });
+}
+
+markdownView.addEventListener('keyup', (event) => {
+  renderMarkdown(event.target.value);
+})
